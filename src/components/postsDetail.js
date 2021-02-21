4d99960a7b47ada10cf01cc2990180d4c11584fc
Mayank Gupta <mgupta.mayank02@gmail.com>
2021-02-21 09:31:20 +0530

    postDetail

--- src/components/postsDetail.js
+++ src/components/postsDetail.js
@@ -1,12 +1,52 @@
 import React,{Component} from 'react';
 
 class PostDetail extends Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+            
+          posts:[],
+          isLoading: false,
+          isError: false
+        }
+      }
+      async componentDidMount() {
+        this.setState({ isLoading: true })
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts/'+this.props.match.params.postID)
+        if (response.ok) {
+          const posts = await response.json()
+         
+          this.setState({ posts:posts, isLoading: false })
+        } else {
+          this.setState({ isError: true, isLoading: false })
+        }
+      }
+      renderPosts = () => {
+       
+        
+                return(
+                    <div>
+                        
+                        <li>Title : {this.state.posts.title}</li>
+                        <ul>{this.state.posts.body}</ul>
+                    </div>  
+                )
+            
+
+          
+      
+      }
+
+
     render(){
-        console.log("posts detail page ");
+
+        
+        
+        
         return(
-            <div>post details page
-                <li>{this.props.match.params.userID}</li>
-                <li>{this.props.match.params.postID}</li>
+            <div>
+                {this.renderPosts()}
+                
             </div>
 
         )
